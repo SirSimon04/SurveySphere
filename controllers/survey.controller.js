1b9616a7b06fad65eefd2f44299dafa9e77d8563
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-05-25 13:12:28 +0200

    update sruvey

--- controllers/survey.controller.js
+++ controllers/survey.controller.js
@@ -1,6 +1,3 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import User from "../models/user.model.js";
 import SurveyModel from "../models/survey.model.js";
 
 export const create = async (req, res) => {
