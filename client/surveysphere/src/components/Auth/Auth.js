fdc52024c4ac19e776fcb04bdc12634e40b9d3ae
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-05-25 15:26:55 +0200

    add auth component

--- client/surveysphere/src/components/Auth/Auth.js
+++ client/surveysphere/src/components/Auth/Auth.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import './Auth.css';
 
-function Auth() {
-    return <p>Auth</p>
-}
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // Implementiere hier die Login-Logik
+  };
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+    // Implementiere hier die Signup-Logik
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          type="password"
+          placeholder="Passwort"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button onClick={handleLogin}>Anmelden</button>
+        <p className="signup-link">
+          Noch kein Konto? <a href="#signup" onClick={handleSignup}>Registrieren</a>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
