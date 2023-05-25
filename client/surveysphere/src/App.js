4c5e1b38b8f082e50e1f1feef02b81bddaa13d14
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-05-25 15:13:35 +0200

    create reacht app and initial setup

---
+++ client/surveysphere/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Auth from "./components/Auth/Auth.js";
+import LandingPage from "./components/LandingPage/LandingPage.js";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Auth />} />
+        <Route path='/overview' element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
