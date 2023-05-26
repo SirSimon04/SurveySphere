86c98a77ae7dd936f28d6343d59047d74b8b1fe8
Mathis Imming <mathis.imming@sap.com>
2023-05-26 10:25:51 +0200

    added return function to button

--- client/surveysphere/src/components/NavBar/NavBar.js
+++ client/surveysphere/src/components/NavBar/NavBar.js
@@ -2,20 +2,24 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 import './NavBar.css';
 
-function navBar() {
-    return (
-        <div className="navBar">
-          <div className="left">
-            <span className='greeting'>Hallo, Martin</span>
-          </div>
-          <div className="middle">
-            <h1>Titel dieser Survey</h1>
-          </div>
-          <div className="right">
-            <button className='cancelButton'>Umfrage verlassen</button>
-          </div>
+const NavBar = () => {
+  const navigate = useNavigate();
+  const handleCancel = (event) => {
+    navigate("/overview");
+  };
+  return (
+      <div className="navBar">
+        <div className="left">
+          <span className='greeting'>Hallo, Martin</span>
         </div>
-      );
+        <div className="middle">
+          <h1>Titel dieser Survey</h1>
+        </div>
+        <div className="right">
+          <button className='cancelButton' onClick={handleCancel}>Umfrage verlassen</button>
+        </div>
+      </div>
+    );
 }
 
-export default navBar
+export default NavBar
