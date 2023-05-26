8d95cf3d679184ff903d74ab8a4c7c55c9cb6726
Mathis Imming <mathis13imming@gmail.com>
2023-05-26 11:10:51 +0200

    adjusted surveyor screen

--- client/surveysphere/src/components/SurveyQuestionCard/SurveyQuestionCard.js
+++ client/surveysphere/src/components/SurveyQuestionCard/SurveyQuestionCard.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import './SurveyQuestionCard.css';
 
 function SurveyQuestionCard() {
   return (
-    <div>SurveyQuestionCard</div>
+    <div className='basicCard'>
+        <p>Frage 1: Welche dieser Farben magst Du am meisten?</p>
+        <div className='answerGrid'>
+            <button className='answer'>Antwort 1</button>
+            <button className='answer'>Antwort 2</button>
+            <button className='answer'>Antwort 3</button>
+            <button className='answer'>Antwort 4</button>
+        </div>
+    </div>
   )
 }
 
--- client/surveysphere/src/components/Surveyor/Surveyor.js
+++ client/surveysphere/src/components/Surveyor/Surveyor.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Surveyor.css'
 import NavBar from './../NavBar/NavBar.js';
+import SurveyQuestionCard from './../SurveyQuestionCard/SurveyQuestionCard.js';
 
 function Surveyor() {
   return (
-    NavBar()
+    <div className='surveyContainer'>
+      <NavBar />
+      <div className='surveyCardContainer'>
+        <SurveyQuestionCard />
+        <SurveyQuestionCard />
+        <SurveyQuestionCard />
+        <SurveyQuestionCard />
+        <SurveyQuestionCard />
+      </div>
+    </div>  
   )
 }
 
