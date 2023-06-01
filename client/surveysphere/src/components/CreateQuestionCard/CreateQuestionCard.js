12e0816d19ac8b50ece295ba271eff0869b98bf3
Mathis Imming <mathis13imming@gmail.com>
2023-06-01 13:21:38 +0200

    added functionality

--- client/surveysphere/src/components/CreateQuestionCard/CreateQuestionCard.js
+++ client/surveysphere/src/components/CreateQuestionCard/CreateQuestionCard.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import './CreateQuestionCard.css'
-
+import React, { useState } from 'react';
+import './CreateQuestionCard.css';
 
 function CreateQuestionCard() {
+  const [question, setQuestion] = useState('');
+  const [answerOptions, setAnswerOptions] = useState(['', '']);
+  const [questionType, setQuestionType] = useState('single');
+
+  const handleQuestionChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const handleAnswerOptionChange = (e, index) => {
+    const updatedOptions = [...answerOptions];
+    updatedOptions[index] = e.target.value;
+    setAnswerOptions(updatedOptions);
+  };
+
+  const addAnswerOption = () => {
+    setAnswerOptions([...answerOptions, '']);
+  };
+
+  const handleQuestionTypeChange = (e) => {
+    setQuestionType(e.target.value);
+  };
+
   return (
     <div className='basicCard'>
-      <input className='answer question' placeholder='Frage eingeben:'></input>
+      <input className='answer question' placeholder='Frage eingeben:' value={question} onChange={handleQuestionChange} />
       <div className='answerGrid'>
-        <input className='answer' placeholder='Antwortmöglichkeit eingeben:'/>
-        <input className='answer' placeholder='Antwortmöglichkeit eingeben:'/>
-        <input className='answer' placeholder='Antwortmöglichkeit eingeben:'/>
-        <input className='answer' placeholder='Antwortmöglichkeit eingeben:'/>
+        {answerOptions.map((option, index) => (
+          <input
+            key={index}
+            className='answer'
+            placeholder='Antwortmöglichkeit eingeben:'
+            value={option}
+            onChange={(e) => handleAnswerOptionChange(e, index)}
+          />
+        ))}
+        <button className='addOptionButton' onClick={addAnswerOption}>+</button>
+      </div>
+      <div className='questionType'>
+        <label>
+          <input type='radio' value='single' checked={questionType === 'single'} onChange={handleQuestionTypeChange} />
+          Single Select
+        </label>
+        <label>
+          <input type='radio' value='multi' checked={questionType === 'multi'} onChange={handleQuestionTypeChange} />
+          Multi Select
+        </label>
       </div>
     </div>
-  )
+  );
 }
 
-export default CreateQuestionCard
+export default CreateQuestionCard;
--- client/surveysphere/src/components/Creator/Creator.js
+++ client/surveysphere/src/components/Creator/Creator.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Creator.css'
 import CreaNavBar from '../CreaNavBar/CreaNavBar.js';
 import CreateQuestionCard from '../CreateQuestionCard/CreateQuestionCard';
 
 function Creator() {
+  const [questionCards, setQuestionCards] = useState([<CreateQuestionCard />]);
+
+  const addQuestionCard = () => {
+    setQuestionCards([...questionCards, <CreateQuestionCard />]);
+  };
+
   return (
     <div className='creatorContainer'>
-        <CreaNavBar />
-        <div className='createCardContainer'>
-          <CreateQuestionCard />
-          <CreateQuestionCard />
-          <CreateQuestionCard />
-          <CreateQuestionCard />
+      <CreaNavBar />
+      <div className='createCardContainer'>
+        {questionCards}
+        <div className='addCardButtonContainer'>
+          <button className='addCardButton' onClick={addQuestionCard}>+</button>
         </div>
+      </div>
     </div>
   )
 }
