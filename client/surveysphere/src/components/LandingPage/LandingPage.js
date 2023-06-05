6d78d81136016c8ac8269efb02cc55b4f43fa936
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-06-05 19:20:05 +0200

    add navigation for landing page

--- client/surveysphere/src/components/LandingPage/LandingPage.js
+++ client/surveysphere/src/components/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect}  from "react";
 import './LandingPage.css';
 import results from './media/results.svg';
 import survey from './media/survey.svg';
+import { useNavigate } from "react-router-dom";
 
 export const AnimatedInput = ({placeholder: passedPlaceholder = "", ...passedProps}) => {
     const [placeholder, setPlaceholder] = useState(passedPlaceholder.slice(0, 0));
@@ -27,6 +28,24 @@ export const AnimatedInput = ({placeholder: passedPlaceholder = "", ...passedPro
 };
 
 function LandingPage() {
+
+    const navigate = useNavigate();
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            const inputValue = event.target.value;
+            navigate(`/surveyor/${inputValue}`);
+        }
+    };
+
+    const onNewClick = () => {
+        navigate('/creator');
+    }
+
+    const onResultClick = () => {
+        navigate('/reviewer');
+    }
+
     return (
     <>
     <div class="page">
@@ -34,16 +53,16 @@ function LandingPage() {
 
         <div class="inputContainer">
             {/* <input class="inputField" type="text" placeholder="Enter ID to join survey"></input>     */}
-            <AnimatedInput class="inputField" placeholder="Enter an ID to join survey"/>
+            <AnimatedInput onKeyDown={handleKeyDown} class="inputField" placeholder="Enter an ID to join survey"/>
         </div>
 
         <div class="cardContainer">
-            <div class="card">
+            <div class="card" onClick={onNewClick}>
                 <h2 class="cardTitle">Create</h2>
                 <img src={survey} alt="Survey"/>
                 <p class="cardContent">Create a new survey</p>
             </div>
-            <div class="card">
+            <div class="card" onClick={onResultClick}>
                 <h2 class="cardTitle">Results</h2>
                 <img src={results} alt="Results"/>
                 <p class="cardContent">Shows results of your survey</p>
