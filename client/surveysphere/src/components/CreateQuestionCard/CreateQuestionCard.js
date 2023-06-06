d98a0dc2d6c459511827f36ead1ce360105f70a5
Mathis Imming <mathis13imming@gmail.com>
2023-06-06 09:21:44 +0200

    new answeroption functionality

--- client/surveysphere/src/components/CreateQuestionCard/CreateQuestionCard.js
+++ client/surveysphere/src/components/CreateQuestionCard/CreateQuestionCard.js
@@ -4,7 +4,7 @@ import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
 
 function CreateQuestionCard({ onDelete }) {
   const [question, setQuestion] = useState('');
-  const [answerOptions, setAnswerOptions] = useState(['', '']);
+  const [answerOptions, setAnswerOptions] = useState(['', '', '']);
   const [singleSelect, setSingleSelect] = useState(true);
 
   const handleQuestionChange = (e) => {
@@ -19,6 +19,12 @@ function CreateQuestionCard({ onDelete }) {
 
   const addAnswerOption = () => {
     setAnswerOptions([...answerOptions, '']);
+  };
+
+  const removeAnswerOption = (index) => {
+    const updatedOptions = [...answerOptions];
+    updatedOptions.splice(index, 1);
+    setAnswerOptions(updatedOptions);
   };
 
   const handleQuestionTypeChange = () => {
@@ -38,13 +44,22 @@ function CreateQuestionCard({ onDelete }) {
       />
       <div className='answerGrid'>
         {answerOptions.map((option, index) => (
-          <input
-            key={index}
-            className='answer'
-            placeholder='Antwortmöglichkeit eingeben:'
-            value={option}
-            onChange={(e) => handleAnswerOptionChange(e, index)}
-          />
+          <div key={index} className='answerContainer'>
+            <input
+              className='answer'
+              placeholder='Antwortmöglichkeit eingeben:'
+              value={option}
+              onChange={(e) => handleAnswerOptionChange(e, index)}
+            />
+            {index >= 2 && (
+              <button
+                className='closeButton'
+                onClick={() => removeAnswerOption(index)}
+              >
+                X
+              </button>
+            )}
+          </div>
         ))}
         <button className='addOptionButton' onClick={addAnswerOption}>
           +
