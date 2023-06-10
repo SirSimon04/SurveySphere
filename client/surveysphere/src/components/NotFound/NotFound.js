40b8104c3927a08732265de7ef3b990f17c22b8a
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2023-06-10 18:32:57 +0200

    add 404

--- client/surveysphere/src/components/NotFound/NotFound.js
+++ client/surveysphere/src/components/NotFound/NotFound.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import notFound from './media/404.svg'
+import './NotFound.css'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="imageContainer">
+        <img src={notFound} alt="Not found" />
+    </div>
   )
 }
 
