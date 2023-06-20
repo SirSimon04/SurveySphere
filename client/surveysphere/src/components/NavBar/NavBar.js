6693b383963dcb88518634e8d7507ff0eccef7a0
Simon Engel <simon.engel01@sap.com>
2023-06-20 15:36:36 +0200

    remove warnings

--- client/surveysphere/src/components/NavBar/NavBar.js
+++ client/surveysphere/src/components/NavBar/NavBar.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import './NavBar.css';
 
-const NavBar = ({ onTitleChange, middle, right, showGreeting = false }) => {
+const NavBar = ({ middle, right, showGreeting = false }) => {
 
   const userName = useSelector(state => state.auth.userName);
-
-  const navigate = useNavigate();
-
-  const handleLogoClick = (event) => {
-    navigate("/overview");
-  };
-
-  const handleLogout = (event) => {
-    console.log("Logout");
-  };
 
   return (
       <div className="navBar">
